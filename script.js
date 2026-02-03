09e30d1e9e7502a5d4f6c71e2a38ec3234a03696
Garrett Gorczyca <ggorczyca@taylorfarms.com>
2026-02-03 13:41:52 -0800

    not blank

---
+++ script.js
@@ -0,0 +1,21 @@
+const map = L.map("map").setView([37.0, -95.0], 4);
+
+// no-label basemap
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png",
+  { attribution: "© OpenStreetMap, © CartoDB" }
+).addTo(map);
+
+fetch("cities.geojson")
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: (feature, layer) => {
+        layer.bindTooltip("Watsonville", {
+          permanent: true,
+          direction: "center",
+          className: "watsonville-label"
+        });
+      }
+    }).addTo(map);
+  });
